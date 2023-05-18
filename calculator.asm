4aa0b6ef054f9929253ee8fb08db9bcf190931ee
huytu13 <89334393+huytu132@users.noreply.github.com>
2023-05-18 18:42:04 +0700

    Update calculator.asm

--- calculator.asm
+++ calculator.asm
@@ -13,7 +13,7 @@ MSG7  DB 10,13,'Enter 2nd Number:$'
 MSG8  DB 10,13,10,13,'The Result is: $' 
 MSG9  DB 10,13, 'Wrong input. Choose again 1 -> 4$'
 MSG   DB 10,13,10,13,'               ***THANK YOU FOR USING MY APP***$'
-ten   DB '               *********Mini Calculator********$'                           
+TENAPP   DB '               *********Mini Calculator********$'                           
 
 .CODE   
 
@@ -25,11 +25,11 @@ MAIN PROC
     MOV DS,AX 
     
     MOV AH,9
-    LEA DX,ten
+    LEA DX,TENAPP
     INT 21H
     
-    call nextLine
-    call nextLine
+    CALL nextLine
+    CALL nextLine
     
 Input:
     
@@ -63,7 +63,7 @@ Input:
     INT 21H
     MOV BH,AL
     SUB BH,48
-
+    
 ;   Logic chon dau vao    
     CMP BH,1
     JE ADD
@@ -82,7 +82,7 @@ Input:
     MOV AH, 9
     INT 21H
     
-    call nextLine; Xuong dong
+    CALL nextLine; Xuong dong
     
     JMP Input; Tro lai buoc nhap input
 
@@ -90,34 +90,34 @@ Input:
     
 ;   ---------Cong 2 so----------    
   ADD:
-    mov bh, 0; Vi o ham main su dung thanh ghi BH, nen o day ta gan no lai bang 0
+    MOV BH, 0; Vi o ham main su dung thanh ghi BH, nen o day ta gan no lai bang 0
         
     LEA DX,MSG6  ;tbao ENTER 1ST NUMBER
     MOV AH,9
     INT 21H 
     
-    call inputDec     ; goi ham nhap so thu nhat he 10
-    push ax           ; luu ax da nhap lai
-    call nextLine
+    CALL inputDec     ; goi ham nhap so thu nhat he 10
+    PUSH AX           ; luu ax da nhap lai
+    CALL nextLine
     
     LEA DX,MSG7    ;tbao ENTER 2ND NUMBER
     MOV AH,9
     INT 21H 
     
-    call inputDec   ; goi ham nhap so thu hai
-    mov bl, al
-    call nextLine  ;Xuong dong
+    CALL inputDec   ; goi ham nhap so thu hai
+    MOV BL, AL
+    CALL nextLine  ;Xuong dong
     
-    pop ax            ; goi ax tu Stack ra
-    call tong         ; goi ham tinh tong 2 so (trong ax va bx)
-    push ax
+    POP AX            ; goi ax tu Stack ra
+    CALL tong         ; goi ham tinh tong 2 so (trong ax va bx)
+    PUSH AX
     
     LEA DX,MSG8    ;in tbao ket qua
     MOV AH,9
     INT 21H
     
-    pop ax
-    call outputDec   ; goi ham in ket qua
+    POP AX
+    CALL outputDec   ; goi ham in ket qua
     
     JMP EXIT_P       ;nhay xuong buoc in ket thuc chuong trinh
 
@@ -127,36 +127,34 @@ Input:
 ;  ----------------------Tru 2 so---------------------    
    SUB: 
    
-    mov bh, 0; Vi o ham main su dung thanh ghi BH, nen o day ta gan no lai bang 0
+    MOV BH, 0; Vi o ham main su dung thanh ghi BH, nen o day ta gan no lai bang 0
         
     LEA DX,MSG6  ;ENTER 1ST NUMBER
     MOV AH,9
     INT 21H 
     
-    call inputDec     ; goi ham nhap so thu nhat he 10
-    push ax           ; luu ax da nhap lai
-    call nextLine     ; Xuong dong
+    CALL inputDec     ; goi ham nhap so thu nhat he 10
+    PUSH AX           ; luu ax da nhap lai
+    CALL nextLine     ; Xuong dong
     
     LEA DX,MSG7    ;ENTER 2ND NUMBER
     MOV AH,9
-    INT 21H 
+    INT 21H  
     
+    CALL inputDec     ; goi ham nhap so thu 2
+    MOV BL, AL
+    CALL nextLine
     
-    
-    call inputDec     ; goi ham nhap so thu 2
-    mov bl, al
-    call nextLine
-    
-    pop ax            ; goi ax tu Stack ra
-    call hieu          ; goi ham tinh hieu 2 so (trong ax va bx)
-    push ax
+    POP AX           ; goi ax tu Stack ra
+    CALL hieu          ; goi ham tinh hieu 2 so (trong ax va bx)
+    PUSH AX
     
     LEA DX,MSG8       ; in tbao ket qua
     MOV AH,9
     INT 21H
     
-    pop ax
-    call outputDec    ; goi ham in ket qua
+    POP AX
+    CALL outputDec    ; goi ham in ket qua
     
     JMP EXIT_P 
     
@@ -165,36 +163,34 @@ Input:
     
    MUL:
  
-    mov bh, 0
+    MOV bh, 0
         
     LEA DX,MSG6  ;ENTER 1ST NUMBER
     MOV AH,9
     INT 21H 
     
-    call inputDec     ; goi CTC nhap so thu nhat he 10
-    push ax           ; luu ax da nhap lai
-    call nextLine
+    CALL inputDec     ; goi CTC nhap so thu nhat he 10
+    PUSH AX           ; luu ax da nhap lai
+    CALL nextLine
     
     LEA DX,MSG7    ;ENTER 2ND NUMBER
     MOV AH,9
     INT 21H 
     
+    CALL inputDec
+    MOV BL, AL
+    CALL nextLine
     
-    
-    call inputDec
-    mov bl, al
-    call nextLine
-    
-    pop ax            ; goi ax tu Stack ra
-    call tich          ; goi ham tinh tich 2 so (trong ax va bx)
-    push ax
+    POP AX            ; goi ax tu Stack ra
+    CALL tich          ; goi ham tinh tich 2 so (trong ax va bx)
+    PUSH AX
     
     LEA DX,MSG8
     MOV AH,9
     INT 21H
     
-    pop ax
-    call outputDec
+    POP AX
+    CALL outputDec
     
     JMP EXIT_P  
     
@@ -205,35 +201,34 @@ Input:
    
    DIV:
     
-    mov bh, 0
+    MOV BH, 0
         
     LEA DX,MSG6  ;ENTER 1ST NUMBER
     MOV AH,9
     INT 21H 
     
-    call inputDec     ; goi CTC nhap so thu nhat he 10
-    push ax           ; luu ax da nhap lai
-    call nextLine
+    CALL inputDec     ; goi CTC nhap so thu nhat he 10
+    PUSH AX           ; luu ax da nhap lai
+    CALL nextLine
     
     LEA DX,MSG7    ;ENTER 2ND NUMBER
     MOV AH,9
     INT 21H 
     
+    CALL inputDec
+    MOV BL, AL
+    CALL nextLine
     
-    call inputDec
-    mov bl, al
-    call nextLine
-    
-    pop ax            ; goi ax tu Stack ra
-    call thuong          ; goi ham tinh thuong 2 so (trong ax va bx)
-    push ax
+    POP AX            ; goi ax tu Stack ra
+    CALL thuong          ; goi ham tinh thuong 2 so (trong ax va bx)
+    PUSH AX
     
     LEA DX,MSG8
     MOV AH,9
     INT 21H
     
-    pop ax
-    call outputDec
+    POP AX
+    CALL outputDec
     
     JMP EXIT_P
     
@@ -243,8 +238,6 @@ Input:
         MOV AH,9
         INT 21H    
         
-    EXIT:
-    
     MOV AH,4CH
     INT 21H
 MAIN ENDP 
@@ -252,139 +245,137 @@ MAIN ENDP
 
 ;    ---------Chuong trinh con-------------  
   
-    tong proc          ; CTC con tinh tong 2 so
-        add ax, bx
-        ret
-    tong endp
+    tong PROC          ; CTC con tinh tong 2 so
+        ADD AX, BX
+        RET
+    tong ENDP
 
 
 ;   Chuong trinh con tinh hieu
-    hieu proc
-        sub ax, bx
-        ret
-    hieu endp  
+    hieu PROC
+        SUB AX, BX
+        RET
+    hieu ENDP  
 
 
 ; Chuong trinh con tinh tich     
-    tich proc 
-         mul bx
-         ret
-    tich endp 
+    tich PROC 
+         MUL BX
+         RET
+    tich ENDP 
 
 
 ; Chuong trinh con tinh thuong    
-    thuong proc 
-         div bl
-         ret
-    thuong endp 
+    thuong PROC 
+         DIV BL
+         RET
+    thuong ENDP 
 
 
 ; Chuong trinh con xuong dong                                                            
-    nextLine proc    
-        mov ah, 9
-        lea dx, CLRF
-        int 21h
-        ret
-    nextLine endp
+    nextLine PROC    
+        MOV AH, 9
+        LEA DX, CLRF
+        INT 21H
+        RET
+    nextLine ENDP
 
 ; Chuong trinh con nhap dau vao he 10     
-    inputDec proc
+    inputDec PROC
          
         batDau:
-            mov bx, 0 ; bien tinh tong
-            mov cx, 0
-            mov ah, 1
-            int 21h
-            cmp al, '-'
-            je dauTru
-            cmp al, '+'
-            je dauCong
-            jmp tiepTuc
+            MOV BX, 0 ; bien tinh tong
+            MOV CX, 0
+            MOV AH, 1
+            INT 21H
+            CMP AL, '-'
+            JE dauTru
+            CMP AL, '+'
+            JE dauCong
+            JMP tiepTuc
              
             dauTru:
-                mov cx, 1
+                MOV CX, 1
              
             dauCong:
-                int 21h
+                INT 21H
              
             tiepTuc:
-                cmp al, '0'
-                jnge khongPhaiSo    ; khong lop hon hoac bang
-                cmp al, '9'
-                jnle khongPhaiSo    ; Khong nho hon hoac bang
-                and ax, 000fh      ; doi thanh chu so, giai thich f sang nhi phan 1111, vd ki tu '1' trong 
+                CMP AL, '0'
+                JNGE khongPhaiSo    ; khong lop hon hoac bang
+                CMP AL, '9'
+                JNLE khongPhaiSo    ; Khong nho hon hoac bang
+                AND AX, 000fh      ; doi thanh chu so, giai thich f sang nhi phan 1111, vd ki tu '1' trong 
                                    ; thanh ghi al co ma hex la 0x31 co ma o cuoi(1) chuyen sang nhi phan la 0001, sau cau lenh and thanh ghi al chua ma 0001 nen ki tu '1' duoc chuyen thanh so
-                push ax             ; luu gia tri vua nhap vao ngan xep
-                mov ax, 10           ; gan ax = 10
+                PUSH AX             ; luu gia tri vua nhap vao ngan xep
+                MOV AX, 10           ; gan ax = 10
                  
-                mul bx              ; ax = tong*10 hay ax = bx * 10
-                mov bx, ax          
-                pop ax              ; lay gia tri vua nhap ra gan vao ax chinh la cai bien 'so' o duoi
-                add bx, ax          ; tong = tong*10 + so
+                MUL BX              ; ax = tong*10 hay ax = bx * 10
+                MOV BX, AX          
+                POP AX              ; lay gia tri vua nhap ra gan vao ax chinh la cai bien 'so' o duoi
+                ADD BX, AX          ; tong = tong*10 + so
                 
-                mov ah, 1
-                int 21h
-                cmp al, 13          ; da enter chua?
-                jne tiepTuc         ; nhap tiep
+                MOV AH, 1
+                INT 21H
+                CMP AL, 13          ; da enter chua?
+                JNE tiepTuc         ; nhap tiep
                  
-                mov ax, bx          ; chuyen KQ ra ax
-                cmp cx,1           ; co phai so am khong
-                jne ra
-                neg ax              ; neu la so am thi doi ax ra so am
+                MOV AX, BX          ; chuyen KQ ra ax
+                CMP CX,1           ; co phai so am khong
+                JNE ra
+                NEG ax              ; neu la so am thi doi ax ra so am
                  
             ra: 
-                ret
+                RET
                  
             khongPhaiSo:
-                mov ah, 2
-                mov dl, 0dh
-                int 21h
-                mov dl, 0ah
-                int 21
-                jmp batDau
+                MOV AH, 2
+                MOV DL, 0dh
+                INT 21H
+                MOV DL, 0ah
+                INT 21H
+                JMP batDau
                  
                  
-    inputDec endp 
+    inputDec ENDP 
      
-    outputDec proc 
-       ; dua vao stack de khong lam thay doi gia tri cua thanh ghi
+    outputDec PROC 
          
-        cmp ax, 0   ;   neu ax > 0 tuc la khong phai so am ta doi ra day
-        jge doiRaDay
-        push ax
-        mov dl, '-'
-        mov ah, 2
-        int 21h
-        pop ax
-        neg ax  ; ax = -ax
+        CMP AX, 0   ;   neu ax >= 0 tuc la khong phai so am ta doi ra day
+        JGE doiRaDay
+        PUSH AX
+        MOV DL, '-'
+        MOV AH, 2
+        INT 21H
+        POP AX
+        NEG AX  ; ax = -ax
          
         doiRaDay:
-            mov cx, 0  ; gan cx = 0
-            mov bx, 10  ; so chia la 10
+            MOV CX, 0  ; gan cx = 0
+            MOV BX, 10  ; so chia la 10
             chia:
-                mov dx, 0  ; gan dx = 0
-                div bx      ; ax = ax / bx; dx = ax % bx
-                push dx      
-                inc cx
-                cmp ax, 0   ; kiem tra xem thuong bang khong chua?
-                jne chia    ; neu khong bang thi lai chia
-                mov ah, 2
+                MOV DX, 0  ; gan dx = 0
+                DIV BX      ; ax = ax / bx; dx = ax % bx
+                PUSH DX      
+                INC CX
+                CMP AX, 0   ; kiem tra xem thuong bang khong chua?
+                JNE chia    ; neu khong bang thi lai chia
+                MOV AH, 2
             hien:
-                pop dx
-                or dl, 30h  ; chuyen tu ki tu sang so hay cong them 48 trong bang ma asci ('0' + 48 = 0)
+                POP DX
+                OR DL, 30H  ; chuyen tu ki tu sang so hay cong them 48 trong bang ma asci ('0' + 48 = 0)
                             ;vd 5 nhi phan 0000 0101
                             ;30 sang nhi phan 0011 0000
                             ;sau cau lenh or ta dc 0011 0101 hay noi cach khac la cong them 48
                             ;do dx la thanh ghi 16 bit va dl la thanh ghi 8 bit nen ta su dung cach nay
-                int 21h
-                loop hien
+                INT 21H
+                LOOP hien
                 
                 ; vi gia tri cac thanh ghi ban dau duoc dua vao stack 
                 ;nen khi thuc hien xong co the lay tu stack va tra lai gia tri ban dau cua cac thanh ghi 
-        ret
+        RET
          
-    outputDec endp
-END MAIN
+    outputDec ENDP
 
 ; su khac biet giua 2 cau lenh and ax, 000fh va or dl,30h mac du chung deu co muc dich la chuyen ki tu thanh so
 ; la and ax, 000fh se xoa tat ca cac bit dau chi giu lai 4 bit cuoi trong he nhi phan, con cau lenh kia chuyen doi gia tri thanh
